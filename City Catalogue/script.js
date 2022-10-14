06efb508b0d0db2ca33b833baee7ec5ec82ad02d
Olamilekan44 <abdulmuizzahmad8@gmail.com>
2022-10-14 03:12:01 +0100

    new changes

---
+++ City Catalogue/script.js
@@ -0,0 +1,93 @@
+<div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/ccpix1.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+        <div class="carousel-caption">
+          <h3>Los Angeles</h3>
+          <p>We had such a great time in LA!</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="img/ccpix2.jpg" alt="Chicago" class="d-block" style="width:100%">
+        <div class="carousel-caption">
+          <h3>Chicago</h3>
+          <p>Thank you, Chicago!</p>
+        </div> 
+      </div>
+      <div class="carousel-item">
+        <img src="img/ccpix3.jpg" alt="New York" class="d-block" style="width:100%">
+        <div class="carousel-caption">
+          <h3>New York</h3>
+          <p>We love the Big Apple!</p>
+        </div>  
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    .overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 1;
+  background-color: #000;
+  opacity: .7;
+}
+
+.body .container-md{
+  position: absolute;
+  top: 30%;
+  margin-left: 10%;
+  z-index: 10;
+  width: 100%;
+}
+
+.body-texts{
+  position: absolute;
+  top: 3rem;
+  width: 100%;
+}
+
+.text-content h1{
+  font-family: 'Work Sans', sans-serif;
+  font-size: 5rem;
+  font-weight: 900;
+  word-spacing: .3rem;
+  color: #fff;
+}
+
+.text-content p{
+  font-size: 1.7rem;
+  color: #2DB6D4;
+}
+
+h1 span{
+  color: #2DB6D4;
+}
+
+p span{
+  color: #fff;
+}
